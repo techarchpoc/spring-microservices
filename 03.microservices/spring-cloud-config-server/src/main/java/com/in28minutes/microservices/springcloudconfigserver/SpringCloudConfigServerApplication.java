d913a02b6bc8f2ee50e54457888d4e7a24824993
techarchpoc <64575898+techarchpoc@users.noreply.github.com>
2020-06-16 13:06:29 +0530

    Update SpringCloudConfigServerApplication.java

--- 03.microservices/spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ 03.microservices/spring-cloud-config-server/src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -8,19 +8,17 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args); 
 		
-		for(int i =0; i <=10; i++) {
+		for(int i =0; i <=20; i++) {
             		System.out.println("*");
         }
 		System.out.println("Spring cloud config server running...");
 		
-		for(int i =0; i <=10; i++) {
+		for(int i =0; i <=20; i++) {
             		System.out.println("*");
         }
-		
-		
-		SpringApplication.run(SpringCloudConfigServerApplication.class, args);              
 
 	}
 }
